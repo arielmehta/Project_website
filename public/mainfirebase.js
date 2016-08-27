51cb1f4e498710b5b381ebe20fcdc901430ab723
arielmehta <mightymeera1@gmail.com>
2016-08-27 16:46:26 -0700

    finished form, pushing to firebase, active traps list, data analysis page created, no charts api yet

--- public/mainfirebase.js
+++ public/mainfirebase.js
@@ -1,3 +1,4 @@
+var trap_number = 0;
 var map;
 function initMap() {
 	var mapDiv = document.getElementById('map');
@@ -19,14 +20,19 @@ var config = {
  	firebase.initializeApp(config);	
  };
 window.onload= function(){
-	for (var keys in Trap_Collection_Form){
-		var lt = Trap_Collection_Form[keys].trap_Latitude;
-		var lg = Trap_Collection_Form[keys].trap_Longitude;
+	for (var keys in Trap_Setup_Form){
+		trap_number = trap_number + 1;
+		console.log(trap_number);
+		var lt = Trap_Setup_Form[keys].Latitude;
+		var lg = Trap_Setup_Form[keys].Longitude;
 		var latitude = Number(lt);
 		var longitude = Number(lg);
 		var position = {lat: latitude, lng: longitude};
-		var title = Trap_Collection_Form[keys].date_Set;
-		var body = ", position: " + Trap_Collection_Form[keys].position_Type + ", Species Found: " + Trap_Collection_Form[keys].egg_Species
+		var month = Trap_Setup_Form[keys].Start_Month;
+		var day = Trap_Setup_Form[keys].Start_Day;
+		var year = Trap_Setup_Form[keys].Start_Year;
+		var title = "Trap Number: " + trap_number + '<div> </div>' + " Team Member: " + Trap_Setup_Form[keys].Initials + '<div></div>';
+		var body = " Date Placed: " + month + "/" + day + "/" + year;
 		var contents = title + body;
 		// console.log(contents);
 		// console.log(position);
@@ -34,10 +40,23 @@ window.onload= function(){
 	};
 }
 
+var urla = "https://zika-project-f48f8.firebaseio.com/Trap_Setup_Form";
+var urlb = "https://zika-project-f48f8.firebaseio.com/Trap_Setup_Form2";
+var urlc = "https://zika-project-f48f8.firebaseio.com/Trap_Setup_Form3";
+var urld = "https://zika-project-f48f8.firebaseio.com/Egg_Data_Form";
+
+
 var url = "https://zika-project-f48f8.firebaseio.com/Trap_Collection_Form";
 
-var firebaseRef = new Firebase(url);
+var firebaseRefa = new Firebase(urla); //first trap set_up form
+var firebaseRefb = new Firebase(urlb); // second trap setup form
+var firebaseRefc = new Firebase(urlc); // third trap setup form
+var firebaseRefd = new Firebase(urld); // egg data found form
 
+
+var firebaseReff = new Firebase(url); // old version for Trap_Collection_Form
+
+//google maps api for home page trap map, function called onload
 function addmarkers(positionval, title, content){
 	var marker = new google.maps.Marker({
 	  position: positionval,
@@ -51,6 +70,7 @@ function addmarkers(positionval, title, content){
 	  infowindow.open(map, marker);
 	});
 };
+
 function writeUserData(){
 	console.log("here");
 	var trap_type = document.getElementById('trap_type').value; //User Input
@@ -66,7 +86,7 @@ function writeUserData(){
 	var Number_Individual_Eggs = document.getElementById('Number_Individual_Eggs').value; //User Input
 	var Egg_Species = document.getElementById('Egg_Species').value; //User Input
 
-	var postRef= firebaseRef.push();// creating form submission id
+	var postRef= firebaseReff.push();// creating form submission id
 	postRef.set({                   //creating children data for each form submission
 		Trap_type: trap_type,  
 		date_Set: Date_Set, 
@@ -85,10 +105,139 @@ function writeUserData(){
 
 var Trap_Collection_Form = {};
 
-firebaseRef.on('value', function(snapshot){
+firebaseReff.on('value', function(snapshot){
 	Trap_Collection_Form = snapshot.val();
 });
   
+function writeStartUserData(){
+	console.log("writing data");
+	var initials = document.getElementById('initials').value;
+	var start_date_month = document.getElementById('datemonth').value;
+	var start_date_day = document.getElementById('dateday').value;
+	var start_date_year = document.getElementById('dateyear').value;
+	var latitude = document.getElementById('lat').value;
+	var longitude = document.getElementById('long').value;
+
+	var postRef= firebaseRefa.push();// creating form submission id
+	postRef.set({                  //creating children data for each form submission
+		Initials: initials,
+		Start_Month: start_date_month,  
+		Start_Day: start_date_day, 
+		Start_Year: start_date_year,
+		Latitude: latitude,
+		Longitude: longitude  
+		}); 
+}
+
+var Trap_Setup_Form = {};
+
+firebaseRefa.on('value', function(snapshot){
+	Trap_Setup_Form = snapshot.val();
+});
+
+
+function writeStartUserData2(){
+	console.log('is this even working');
+	var modifications = document.getElementById('modificationss').value;
+	var leaf_added_type = document.getElementById('leaff').value;
+	var flower_added_type = document.getElementById('flowerr').value;
+	var compost_added_type = document.getElementById('compostt').value;
+	var other_natural_modif = document.getElementById('otherr').value;
+	var fragrance = document.getElementById('fragrancee').value;
+	var human_scents = document.getElementById('clothingg').value;
+	var other_synthetic_mod = document.getElementById('otherss').value;
+	var color_change = document.getElementById('colorss').value;
+	var pattern_change = document.getElementById('patternss').value;
+
+	var postRef= firebaseRefb.push();// creating form submission id
+	postRef.set({ 
+		Trap_Modifications: modifications,
+		Leaves_Added: leaf_added_type,
+		Flowers_Added: flower_added_type,
+		Compost_Added: compost_added_type,
+		Other_Natural_Modifications: other_natural_modif,
+		Scents : fragrance,
+		Human_Scents: human_scents,
+		Other_Synthetic_Modifications: other_synthetic_mod,
+		Color_Change: color_change,
+		Pattern_Change: pattern_change
+	}); 
+}
+var Trap_Setup_Form2 = {};
+
+firebaseRefb.on('value', function(snapshot){
+	Trap_Setup_Form2 = snapshot.val();
+});
+
+
+
+
+ function writeStartUserData3(){
+ 	var surrounding_plants = document.getElementById('plantts').value;
+	var surrounding_water = document.getElementById('waters').value;
+	var surrounding_shade = document.getElementById('shady').value;
+	var surrounding_sun = document.getElementById('sunnyy').value;
+	var surrounding_dry = document.getElementById('dryy').value;
+	var surrounding_wet = document.getElementById('wett').value;
+
+	var postRef= firebaseRefc.push();// creating form submission id
+	postRef.set({
+		Nearby_Plants: surrounding_plants,
+		Nearby_Water: surrounding_water,
+		Shade: surrounding_shade,
+		Sun: surrounding_sun,
+		Dry: surrounding_dry,
+		Wet: surrounding_wet
+	});
+ }
+var Trap_Setup_Form3 = {};
+
+firebaseRefc.on('value', function(snapshot){
+	Trap_Setup_Form3 = snapshot.val();
+});
+
+
+function writeFoundEggsUserData(){
+	console.log('writing user data');
+	var trap_number = document.getElementById('trap_number').value;
+	var month_Eggs_Found = document.getElementById('month_found').value;
+	var day_Eggs_Found = document.getElementById('day_found').value;
+	var year_Eggs_Found = document.getElementById('year_found').value;
+	var temp = document.getElementById('temp').value;
+	var humidity = document.getElementById('humid').value;
+	var rainfall = document.getElementById('rain').value;
+	var number_eggs_total = document.getElementById('totall').value;
+	var number_egg_rafts = document.getElementById('raftss').value;
+	var number_individual_eggs = document.getElementById('eggss').value;
+	var egg_species = document.getElementById('speciess').value;
+
+	var postRef= firebaseRefd.push();// creating form submission id
+	postRef.set({                   //creating children data for each form submission
+		Trap_Number: trap_number,  
+		Mounth_Found: month_Eggs_Found, 
+		Day_Found: day_Eggs_Found,
+		Year_Found: year_Eggs_Found,
+		Temperature: temp,  
+		Humidity: humidity, 
+		Rainfall: rainfall,
+		Eggs_Total: number_eggs_total,
+		Egg_Rafts: number_egg_rafts,
+		Individual_Eggs: number_individual_eggs,
+		Egg_Species: egg_species  
+		}); 
+}
+
+var Egg_Data_Form = {};
+
+firebaseRefd.on('value', function(snapshot){
+	Egg_Data_Form = snapshot.val();
+});
+
+
+
+
+
+
 
 
 
--- public/weatherdata.js
+++
@@ -1,5 +0,0 @@
-
-var BaseUrl;
-
-curl -H "token:<iKrVOoXoEMwBeKrIqUioNVTRIIvYRkYC>" "url"
-	$.ajax({ url:<url>, data:{<data>}, headers:{ token:<iKrVOoXoEMwBeKrIqUioNVTRIIvYRkYC> } })
